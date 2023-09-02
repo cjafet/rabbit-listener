1cb2568cc56a1cbe7d66d8f45585df72b0102e0d
cjafet <cjafet07@gmail.com>
2023-09-02 11:29:29 -0300

    Initial commit

---
+++ src/test/java/com/cjafet/rabbitlistener/RabbitListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cjafet.rabbitlistener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitListenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
